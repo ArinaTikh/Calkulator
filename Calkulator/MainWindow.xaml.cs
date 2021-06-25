7ac532d03961c42bd80c10bac12411f4d9e03734
Arina Tikhonova <tikhonovaarina2000@gmail.com>
2021-06-25 12:10:59 +0300

    Подсчет всего и сумы выплат

--- Calkulator/MainWindow.xaml.cs
+++ Calkulator/MainWindow.xaml.cs
@@ -8,26 +8,46 @@ namespace Calkulator
     /// </summary>
     public partial class MainWindow : Window
     {
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void money_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void days_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void procent_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+        private void rezult_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+        private void rezult_TextChanged_1(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+        public void Button_Click(object sender, RoutedEventArgs e)
         {
             int cash = Convert.ToInt32(money);
             int day = Convert.ToInt32(days);
             int proc = Convert.ToInt32(procent);
-             if (cash > 500000)
+            if (cash < 500000)
             {
-                if ( day >365)
+                if (day > 365)
                 {
                     MessageBox.Show("Параметры введины неверно");
                 }
-            }
-            int numb = proc * 100;
-            for (numb = 0; numb < 100; numb++) ;
-            {
-                numb = numb + numb;
+
             }
 
-                
+            int rezvsego = cash * day * proc;
+            int rezsum = rezvsego - cash;
 
         }
+
     }
 }
